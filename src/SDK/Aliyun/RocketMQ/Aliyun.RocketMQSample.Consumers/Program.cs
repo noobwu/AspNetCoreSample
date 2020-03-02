d51247212aee7dfdbe3256ef2c28c826326dbc9d
guoguo <noobwu@126.com>
2020-03-03 07:16:05 +0800

    RocketMQ
    RocketMQ

--- src/SDK/Aliyun/RocketMQ/Aliyun.RocketMQSample.Consumers/Program.cs
+++ src/SDK/Aliyun/RocketMQ/Aliyun.RocketMQSample.Consumers/Program.cs
@@ -31,7 +31,7 @@ using System.Threading.Tasks;
 /// <summary>
 /// The Consumer namespace.
 /// </summary>
-namespace Aliyun.RocketMQSample.OrderConsumer
+namespace Aliyun.RocketMQSample.Consumers
 {
     /// <summary>
     /// Class Program.
@@ -47,106 +47,30 @@ namespace Aliyun.RocketMQSample.OrderConsumer
         {
             try
             {
-                Console.Title = "KmmpRocketMQOrderReceiverTest";
+                Console.Title = "KmmpRocketMQReceiverTest";
                 //KmmpMQConsumerTest();
-                KmmpRocketMQOrderReceiverTest();
+                KmmpRocketMQReceiverTest();
+
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
             }
+
             Console.ReadKey();
         }
-        /// <summary>
-        /// KMMPs the mq consumer test.
-        /// </summary>
-        static void KmmpMQConsumerTest()
-        {
-            /// <summary>
-            /// 消费都数量
-            /// </summary>
-            const int ConsumerCount = 2;
-            string queueName = "CateringVipType";
-            Console.WriteLine($"接收消息,{queueName}:{DateTime.Now}");
 
-            KmmpMQReceiverTest(queueName);
-            string tempQueueName = "TempCateringVipType";
-            Console.WriteLine($"接收消息,{tempQueueName}:{DateTime.Now}");
-
-            KmmpMQReceiverTest(tempQueueName);
-        }
-
-        /// <summary>
-        /// KMMPs the mq publisher test.
-        /// </summary>
-        /// <param name="queueName">Name of the queue.</param>
-        static void KmmpMQReceiverTest(string queueName)
-        {
-            try
-            {
-                var receiver = GetReceiver(queueName);
-                receiver.Received += (sender, args) =>
-                {
-                    //Execute(q.Value<string>("Method"), args.Message);
-                    //Console.WriteLine($"args:{JsonHelper.JsonConvertSerialize(args)}");
-                    var mqData = args.Message as MQ_VipData<Temp_VipType>;
-                    if (mqData == null)
-                    {
-                        mqData = JsonHelper.JsonConvertDeserialize<MQ_VipData<Temp_VipType>>(args.Message.ToString());
-                    }
-                    new SyncVipTypeMqReceiver().Execute(mqData);
-                    Console.WriteLine($"ChannelName:{args.ChannelName},MessageId:{mqData?.MessageId}");
-                };
-                receiver.Start();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-        }
-
-        /// <summary>
-        /// Author：tiny.wu
-        /// Date：2016-09-28
-        /// Desc：执行
-        /// </summary>
-        /// <param name="typeName">Name of the type.</param>
-        /// <param name="msg">The MSG.</param>
-        private void Execute(string typeName, object msg)
-        {
-            Type type = Type.GetType(typeName);
-            var method = type.GetMethod("Execute");
-            method.Invoke(Activator.CreateInstance(type), new[] { msg });
-        }
-
-        /// <summary>
-        /// 获取消息接收者
-        /// </summary>
-        /// <param name="queueName">Name of the queue.</param>
-        /// <returns>IMessageReceiver.</returns>
-        /// <exception cref="System.Exception">0006</exception>
-        /// <exception cref="Exception">0006</exception>
-        private static IMessageReceiver GetReceiver(string queueName)
-        {
-            var messageQueue = MessageQueueHelper.GetMessageQueueFromPool(queueName);
-            var receiver = messageQueue.GetMessageReceiver(queueName, null);
-            if (receiver == null)
-            {
-                throw new Exception("0006");
-            }
-            return receiver;
-        }
         /// <summary>
         /// Consumers the test.
         /// </summary>
-        static void KmmpRocketMQOrderReceiverTest()
+        static void KmmpRocketMQReceiverTest()
         {
             Console.WriteLine($"KmmpRocketMQReceiverTest,开始:{DateTime.Now}");
             string strRocketMQConfigs = JsonConfigInfo.ReadAllFromFile("RocketMQConfigs.json");
             List<RocketMQConfig> configs = JsonHelper.JsonConvertDeserialize<List<RocketMQConfig>>(strRocketMQConfigs);
             var stopWatch = new Stopwatch();
             stopWatch.Start();
-            configs = configs.Where(a => (new byte[] { 2, 3 }).Contains(a.MsgType)).ToList();
+            configs = configs.Where(a => !(new byte[] { 2, 3 }).Contains(a.MsgType)).ToList();
             configs?.ForEach(config =>
             {
                 string queueName = $"{config.GroupId.Replace(config.GroupIdPrefix, string.Empty)}";
@@ -177,6 +101,72 @@ namespace Aliyun.RocketMQSample.OrderConsumer
             Console.WriteLine($"KmmpRocketMQReceiverTest,结束, 使用时间{stopWatch.ElapsedMilliseconds}毫秒");
         }
         /// <summary>
+        /// KMMPs the mq consumer test.
+        /// </summary>
+        static void KmmpMQConsumerTest()
+        {
+            /// <summary>
+            /// 消费都数量
+            /// </summary>
+            const int ConsumerCount = 2;
+            string queueName = "CateringVipType";
+            Console.WriteLine($"接收消息,{queueName}:{DateTime.Now}");
+
+            KmmpMQReceiverTest(queueName);
+            string tempQueueName = "TempCateringVipType";
+            Console.WriteLine($"接收消息,{tempQueueName}:{DateTime.Now}");
+
+            KmmpMQReceiverTest(tempQueueName);
+        }
+
+        /// <summary>
+        /// KMMPs the mq publisher test.
+        /// </summary>
+        /// <param name="queueName">Name of the queue.</param>
+        static void KmmpMQReceiverTest(string queueName)
+        {
+            try
+            {
+                var receiver = GetReceiver(queueName);
+                StartKmmpMQReceiver(receiver);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        /// <summary>
+        /// Author：tiny.wu
+        /// Date：2016-09-28
+        /// Desc：执行
+        /// </summary>
+        /// <param name="typeName">Name of the type.</param>
+        /// <param name="msg">The MSG.</param>
+        private void Execute(string typeName, object msg)
+        {
+            Type type = Type.GetType(typeName);
+            var method = type.GetMethod("Execute");
+            method.Invoke(Activator.CreateInstance(type), new[] { msg });
+        }
+
+        /// <summary>
+        /// 获取消息接收者
+        /// </summary>
+        /// <param name="queueName">Name of the queue.</param>
+        /// <returns>IMessageReceiver.</returns>
+        /// <exception cref="Exception">0006</exception>
+        private static IMessageReceiver GetReceiver(string queueName)
+        {
+            var messageQueue = MessageQueueHelper.GetMessageQueueFromPool(queueName);
+            var receiver = messageQueue.GetMessageReceiver(queueName, null);
+            if (receiver == null)
+            {
+                throw new Exception("0006");
+            }
+            return receiver;
+        }
+        /// <summary>
         /// KMMPs the mq publisher test.
         /// </summary>
         /// <param name="receiver">The receiver.</param>
@@ -189,10 +179,6 @@ namespace Aliyun.RocketMQSample.OrderConsumer
                     //Execute(q.Value<string>("Method"), args.Message);
                     //Console.WriteLine($"args:{JsonHelper.JsonConvertSerialize(args)}");
                     var mqData = args.Message as MQ_VipData<Temp_VipType>;
-                    if (mqData == null)
-                    {
-                        mqData = JsonHelper.JsonConvertDeserialize<MQ_VipData<Temp_VipType>>(args.Message.ToString());
-                    }
                     new SyncVipTypeMqReceiver().Execute(mqData);
                     Console.WriteLine($"ChannelName:{args.ChannelName},MessageId:{mqData?.MessageId}");
                 };
@@ -203,6 +189,5 @@ namespace Aliyun.RocketMQSample.OrderConsumer
                 Console.WriteLine(ex);
             }
         }
-
     }
 }
