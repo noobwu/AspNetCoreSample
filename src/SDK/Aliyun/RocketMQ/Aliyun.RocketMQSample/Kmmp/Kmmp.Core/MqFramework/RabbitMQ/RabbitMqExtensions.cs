0d03445e84d0b07cffae000fd0c4d332295c75bd
guoguo <noobwu@126.com>
2020-03-08 13:45:38 +0800

    RabbitMQ
    RabbitMQ

---
+++ src/SDK/Aliyun/RocketMQ/Aliyun.RocketMQSample/Kmmp/Kmmp.Core/MqFramework/RabbitMQ/RabbitMqExtensions.cs
@@ -0,0 +1,85 @@
+﻿// ***********************************************************************
+// Assembly         : Aliyun.RocketMQSample
+// Author           : Administrator
+// Created          : 2020-03-08
+//
+// Last Modified By : Administrator
+// Last Modified On : 2020-03-08
+// ***********************************************************************
+// <copyright file="RabbitMqExtensions.cs" company="NoobCore.com">
+//     Copyright ©  2020
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// The RabbitMQ namespace.
+/// </summary>
+namespace Kmmp.Core.MqFramework.RabbitMQ
+{
+    /// <summary>
+    /// Class RabbitMqExtensions.
+    /// </summary>
+    public static class RabbitMqExtensions
+    {
+        /// <summary>
+        /// Opens the channel.
+        /// </summary>
+        /// <param name="connection">The connection.</param>
+        /// <returns>IModel.</returns>
+        public static IModel OpenChannel(this IConnection connection)
+        {
+            var channel = connection.CreateModel();
+            channel.RegisterDirectExchange();
+            channel.RegisterDlqExchange();
+            channel.RegisterTopicExchange();
+            return channel;
+        }
+
+        /// <summary>
+        /// Registers the direct exchange.
+        /// </summary>
+        /// <param name="channel">The channel.</param>
+        /// <param name="exchangeName">Name of the exchange.</param>
+        public static void RegisterDirectExchange(this IModel channel, string exchangeName = null)
+        {
+            channel.ExchangeDeclare(exchangeName ?? QueueNames.Exchange, "direct", durable: true, autoDelete: false, arguments: null);
+        }
+
+        /// <summary>
+        /// Registers the DLQ exchange.
+        /// </summary>
+        /// <param name="channel">The channel.</param>
+        /// <param name="exchangeName">Name of the exchange.</param>
+        public static void RegisterDlqExchange(this IModel channel, string exchangeName = null)
+        {
+            channel.ExchangeDeclare(exchangeName ?? QueueNames.ExchangeDlq, "direct", durable: true, autoDelete: false, arguments: null);
+        }
+
+        /// <summary>
+        /// Registers the topic exchange.
+        /// </summary>
+        /// <param name="channel">The channel.</param>
+        /// <param name="exchangeName">Name of the exchange.</param>
+        public static void RegisterTopicExchange(this IModel channel, string exchangeName = null)
+        {
+            channel.ExchangeDeclare(exchangeName ?? QueueNames.ExchangeTopic, "topic", durable: false, autoDelete: false, arguments: null);
+        }
+
+        /// <summary>
+        /// Registers the fanout exchange.
+        /// </summary>
+        /// <param name="channel">The channel.</param>
+        /// <param name="exchangeName">Name of the exchange.</param>
+        public static void RegisterFanoutExchange(this IModel channel, string exchangeName)
+        {
+            channel.ExchangeDeclare(exchangeName, "fanout", durable: false, autoDelete: false, arguments: null);
+        }
+    }
+}
---
+++ src/SDK/Aliyun/RocketMQ/Aliyun.RocketMQSample/Kmmp/Kmmp.Core/MqFramework/RabbitMQ/RabbitMqProducer.cs
@@ -0,0 +1,100 @@
+﻿// ***********************************************************************
+// Assembly         : Aliyun.RocketMQSample
+// Author           : Administrator
+// Created          : 2020-03-08
+//
+// Last Modified By : Administrator
+// Last Modified On : 2020-03-08
+// ***********************************************************************
+// <copyright file="RabbitMqProducer.cs" company="NoobCore.com">
+//     Copyright ©  2020
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// The RabbitMQ namespace.
+/// </summary>
+namespace Kmmp.Core.MqFramework.RabbitMQ
+{
+    /// <summary>
+    /// Class RabbitMqProducer.
+    /// </summary>
+    public class RabbitMqProducer
+    {
+        /// <summary>
+        /// The MSG factory
+        /// </summary>
+        protected readonly RabbitMqMessageFactory msgFactory;
+        /// <summary>
+        /// Gets or sets the retry count.
+        /// </summary>
+        /// <value>The retry count.</value>
+        public int RetryCount { get; set; }
+
+        //http://www.rabbitmq.com/blog/2012/04/25/rabbitmq-performance-measurements-part-2/
+        //http://www.rabbitmq.com/amqp-0-9-1-reference.html
+        /// <summary>
+        /// Gets or sets the prefetch count.
+        /// </summary>
+        /// <value>The prefetch count.</value>
+        public ushort PrefetchCount { get; set; } = 20;
+        /// <summary>
+        /// The connection
+        /// </summary>
+        private IConnection connection;
+        /// <summary>
+        /// Gets the connection.
+        /// </summary>
+        /// <value>The connection.</value>
+        public IConnection Connection
+        {
+            get
+            {
+                if (connection == null)
+                {
+                    connection = msgFactory.ConnectionFactory.CreateConnection();
+                }
+                return connection;
+            }
+        }
+
+        /// <summary>
+        /// The channel
+        /// </summary>
+        private IModel channel;
+        /// <summary>
+        /// Gets the channel.
+        /// </summary>
+        /// <value>The channel.</value>
+        public IModel Channel
+        {
+            get
+            {
+                if (channel == null || !channel.IsOpen)
+                {
+                    channel = Connection.OpenChannel();
+                    //prefetch size is no supported by RabbitMQ
+                    //http://www.rabbitmq.com/specification.html#method-status-basic.qos
+                    channel.BasicQos(prefetchSize: 0, prefetchCount: PrefetchCount, global: false);
+                }
+                return channel;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RabbitMqProducer"/> class.
+        /// </summary>
+        /// <param name="msgFactory">The MSG factory.</param>
+        public RabbitMqProducer(RabbitMqMessageFactory msgFactory)
+        {
+            this.msgFactory = msgFactory;
+        }
+    }
+}
