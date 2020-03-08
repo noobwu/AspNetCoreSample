de87d9c89f33d22a7ae539e1cfeb3be500a7bef6
guoguo <noobwu@126.com>
2020-03-08 17:53:20 +0800

    UnitTests
    UnitTests

---
+++ src/SDK/Aliyun/RocketMQ/Aliyun.RocketMQSample.UnitTests/Messaging/RabbitMqTests.cs
@@ -0,0 +1,174 @@
+﻿// ***********************************************************************
+// Assembly         : Aliyun.RocketMQSample.UnitTests
+// Author           : Administrator
+// Created          : 2020-03-08
+//
+// Last Modified By : Administrator
+// Last Modified On : 2020-03-08
+// ***********************************************************************
+// <copyright file="RabbitMqTests.cs" company="NoobCore.com">
+//     Copyright ©  2020
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using Kmmp.Core.MqFramework;
+using Kmmp.Core.MqFramework.RabbitMQ;
+using NUnit.Framework;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kmmp.Core.Extension;
+using System.Threading;
+
+/// <summary>
+/// The Messaging namespace.
+/// </summary>
+namespace Aliyun.RocketMQSample.UnitTests.Messaging
+{
+    /// <summary>
+    /// Class HelloRabbit.
+    /// </summary>
+    public class HelloRabbit
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name { get; set; }
+    }
+    /// <summary>
+    /// Class RabbitMqTests.
+    /// </summary>
+    public class RabbitMqTests
+    {
+        /// <summary>
+        /// The mq factory
+        /// </summary>
+        private readonly ConnectionFactory mqFactory = new ConnectionFactory
+        {
+            HostName = Environment.GetEnvironmentVariable("CI_RABBITMQ") ?? "localhost"
+        };
+        /// <summary>
+        /// The exchange
+        /// </summary>
+        private const string Exchange = "mq:tests";
+        /// <summary>
+        /// The exchange DLQ
+        /// </summary>
+        private const string ExchangeDlq = "mq:tests.dlq";
+        /// <summary>
+        /// The exchange topic
+        /// </summary>
+        private const string ExchangeTopic = "mq:tests.topic";
+        /// <summary>
+        /// The exchange fanout
+        /// </summary>
+        private const string ExchangeFanout = "mq:tests.fanout";
+        /// <summary>
+        /// Tests the fixture set up.
+        /// </summary>
+
+        [OneTimeSetUp]
+        public void TestFixtureSetUp()
+        {
+            using (IConnection connection = mqFactory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.RegisterDirectExchange(Exchange);
+                channel.RegisterDlqExchange(ExchangeDlq);
+                channel.RegisterTopicExchange(ExchangeTopic);
+
+                RegisterQueue(channel, QueueNames<HelloRabbit>.In);
+                RegisterQueue(channel, QueueNames<HelloRabbit>.Priority);
+                RegisterDlq(channel, QueueNames<HelloRabbit>.Dlq);
+                RegisterTopic(channel, QueueNames<HelloRabbit>.Out);
+                RegisterQueue(channel, QueueNames<HelloRabbit>.In, exchange: ExchangeTopic);
+
+                channel.PurgeQueue<HelloRabbit>();
+            }
+        }
+
+        /// <summary>
+        /// Registers the queue.
+        /// </summary>
+        /// <param name="channel">The channel.</param>
+        /// <param name="queueName">Name of the queue.</param>
+        /// <param name="exchange">The exchange.</param>
+        public static void RegisterQueue(IModel channel, string queueName, string exchange = Exchange)
+        {
+            var args = new Dictionary<string, object> {
+                {"x-dead-letter-exchange", ExchangeDlq },
+                {"x-dead-letter-routing-key", queueName.Replace(".inq",".dlq").Replace(".priorityq",".dlq") },
+            };
+            channel.QueueDeclare(queueName, durable: true, exclusive: false, autoDelete: false, arguments: args);
+            channel.QueueBind(queueName, exchange, routingKey: queueName);
+        }
+
+        /// <summary>
+        /// Registers the topic.
+        /// </summary>
+        /// <param name="channel">The channel.</param>
+        /// <param name="queueName">Name of the queue.</param>
+        public static void RegisterTopic(IModel channel, string queueName)
+        {
+            channel.QueueDeclare(queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+            channel.QueueBind(queueName, ExchangeTopic, routingKey: queueName);
+        }
+
+        /// <summary>
+        /// Registers the DLQ.
+        /// </summary>
+        /// <param name="channel">The channel.</param>
+        /// <param name="queueName">Name of the queue.</param>
+        public static void RegisterDlq(IModel channel, string queueName)
+        {
+            channel.QueueDeclare(queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+            channel.QueueBind(queueName, ExchangeDlq, routingKey: queueName);
+        }
+
+        /// <summary>
+        /// Exchanges the delete.
+        /// </summary>
+        /// <param name="channel">The channel.</param>
+        /// <param name="exchange">The exchange.</param>
+        public void ExchangeDelete(IModel channel, string exchange)
+        {
+            try
+            {
+                channel.ExchangeDelete(exchange);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error ExchangeDelete(): {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Defines the test method Can_publish_messages_to_RabbitMQ.
+        /// </summary>
+        [Test]
+        public void Can_publish_messages_to_RabbitMQ()
+        {
+            using (IConnection connection = mqFactory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                5.Times(i =>
+                {
+                    byte[] payload = new HelloRabbit { Name = "World! #{0}".Fmt(i) }.ToJson().ToUtf8Bytes();
+                    var props = channel.CreateBasicProperties();
+                    props.Persistent = true;
+
+                    channel.BasicPublish(exchange: Exchange,
+                        routingKey: QueueNames<HelloRabbit>.In, basicProperties: props, body: payload);
+
+                    Console.WriteLine("Sent Message " + i);
+                    Thread.Sleep(1000);
+                });
+            }
+        }
+
+    }
+}
---
+++ src/SDK/Aliyun/RocketMQ/Aliyun.RocketMQSample/Kmmp/Kmmp.Core/Extension/IntExtensions.cs
@@ -0,0 +1,135 @@
+﻿// ***********************************************************************
+// Assembly         : Aliyun.RocketMQSample
+// Author           : Administrator
+// Created          : 2020-03-08
+//
+// Last Modified By : Administrator
+// Last Modified On : 2020-03-08
+// ***********************************************************************
+// <copyright file="IntExtensions.cs" company="NoobCore.com">
+//     Copyright ©  2020
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// The Extension namespace.
+/// </summary>
+namespace Kmmp.Core.Extension
+{
+    /// <summary>
+    /// Class IntExtensions.
+    /// </summary>
+    public static class IntExtensions
+    {
+        /// <summary>
+        /// Timeses the specified times.
+        /// </summary>
+        /// <param name="times">The times.</param>
+        /// <returns>IEnumerable&lt;System.Int32&gt;.</returns>
+        public static IEnumerable<int> Times(this int times)
+        {
+            for (var i = 0; i < times; i++)
+            {
+                yield return i;
+            }
+        }
+
+        /// <summary>
+        /// Timeses the specified action function.
+        /// </summary>
+        /// <param name="times">The times.</param>
+        /// <param name="actionFn">The action function.</param>
+        public static void Times(this int times, Action<int> actionFn)
+        {
+            for (var i = 0; i < times; i++)
+            {
+                actionFn(i);
+            }
+        }
+
+        /// <summary>
+        /// Timeses the specified action function.
+        /// </summary>
+        /// <param name="times">The times.</param>
+        /// <param name="actionFn">The action function.</param>
+        public static void Times(this int times, Action actionFn)
+        {
+            for (var i = 0; i < times; i++)
+            {
+                actionFn();
+            }
+        }
+
+        /// <summary>
+        /// Timeses the specified action function.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="times">The times.</param>
+        /// <param name="actionFn">The action function.</param>
+        /// <returns>List&lt;T&gt;.</returns>
+        public static List<T> Times<T>(this int times, Func<T> actionFn)
+        {
+            var list = new List<T>();
+            for (var i = 0; i < times; i++)
+            {
+                list.Add(actionFn());
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// Timeses the specified action function.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="times">The times.</param>
+        /// <param name="actionFn">The action function.</param>
+        /// <returns>List&lt;T&gt;.</returns>
+        public static List<T> Times<T>(this int times, Func<int, T> actionFn)
+        {
+            var list = new List<T>();
+            for (var i = 0; i < times; i++)
+            {
+                list.Add(actionFn(i));
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// Timeses the asynchronous.
+        /// </summary>
+        /// <param name="times">The times.</param>
+        /// <param name="actionFn">The action function.</param>
+        /// <returns>List&lt;IAsyncResult&gt;.</returns>
+        public static List<IAsyncResult> TimesAsync(this int times, Action<int> actionFn)
+        {
+            var asyncResults = new List<IAsyncResult>(times);
+            for (var i = 0; i < times; i++)
+            {
+                asyncResults.Add(actionFn.BeginInvoke(i, null, null));
+            }
+            return asyncResults;
+        }
+
+        /// <summary>
+        /// Timeses the asynchronous.
+        /// </summary>
+        /// <param name="times">The times.</param>
+        /// <param name="actionFn">The action function.</param>
+        /// <returns>List&lt;IAsyncResult&gt;.</returns>
+        public static List<IAsyncResult> TimesAsync(this int times, Action actionFn)
+        {
+            var asyncResults = new List<IAsyncResult>(times);
+            for (var i = 0; i < times; i++)
+            {
+                asyncResults.Add(actionFn.BeginInvoke(null, null));
+            }
+            return asyncResults;
+        }
+    }
+}
