e74c5016ac1866b20252c7937bb5d4e9c38e3674
guoguo <noobwu@126.com>
2020-03-08 13:16:15 +0800

    RabbitMQ
    RabbitMQ

---
+++ src/SDK/Aliyun/RocketMQ/Aliyun.RocketMQSample/Kmmp/Kmmp.Core/Extension/StringExtensions.cs
@@ -0,0 +1,105 @@
+﻿// ***********************************************************************
+// Assembly         : Aliyun.RocketMQSample
+// Author           : Administrator
+// Created          : 2020-03-08
+//
+// Last Modified By : Administrator
+// Last Modified On : 2020-03-08
+// ***********************************************************************
+// <copyright file="StringExtensions.cs" company="NoobCore.com">
+//     Copyright ©  2020
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// The Extension namespace.
+/// </summary>
+namespace Kmmp.Core.Extension
+{
+    /// <summary>
+    /// Class StringExtensions.
+    /// </summary>
+    public static class StringExtensions
+    {
+
+        /// <summary>
+        /// Converts the string representation of a number to an integer.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <returns>System.Int32.</returns>
+        public static int ToInt(this string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return default(int);
+            }
+            return int.Parse(text);
+        }
+
+        /// <summary>
+        /// Converts the string representation of a number to an integer.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns>System.Int32.</returns>
+        public static int ToInt(this string text, int defaultValue)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return defaultValue;
+            }
+            int ret;
+            return int.TryParse(text, out ret) ? ret : defaultValue;
+        }
+        /// <summary>
+        /// Splits the on first.
+        /// </summary>
+        /// <param name="strVal">The string value.</param>
+        /// <param name="needle">The needle.</param>
+        /// <returns>System.String[].</returns>
+        public static string[] SplitOnFirst(this string strVal, char needle)
+        {
+            if (strVal == null) return new string[0];
+            var pos = strVal.IndexOf(needle);
+            return pos == -1
+                ? new[] { strVal }
+                : new[] { strVal.Substring(0, pos), strVal.Substring(pos + 1) };
+        }
+
+        /// <summary>
+        /// Splits the on first.
+        /// </summary>
+        /// <param name="strVal">The string value.</param>
+        /// <param name="needle">The needle.</param>
+        /// <returns>System.String[].</returns>
+        public static string[] SplitOnFirst(this string strVal, string needle)
+        {
+            if (strVal == null) return new string[0];
+            var pos = strVal.IndexOf(needle, StringComparison.OrdinalIgnoreCase);
+            return pos == -1
+                ? new[] { strVal }
+                : new[] { strVal.Substring(0, pos), strVal.Substring(pos + needle.Length) };
+        }
+
+        /// <summary>
+        /// Splits the on last.
+        /// </summary>
+        /// <param name="strVal">The string value.</param>
+        /// <param name="needle">The needle.</param>
+        /// <returns>System.String[].</returns>
+        public static string[] SplitOnLast(this string strVal, char needle)
+        {
+            if (strVal == null) return new string[0];
+            var pos = strVal.LastIndexOf(needle);
+            return pos == -1
+                ? new[] { strVal }
+                : new[] { strVal.Substring(0, pos), strVal.Substring(pos + 1) };
+        }
+    }
+}
---
+++ src/SDK/Aliyun/RocketMQ/Aliyun.RocketMQSample/Kmmp/Kmmp.Core/MqFramework/RabbitMQ/RabbitMqMessageFactory.cs
@@ -0,0 +1,104 @@
+﻿// ***********************************************************************
+// Assembly         : Aliyun.RocketMQSample
+// Author           : Administrator
+// Created          : 2020-03-08
+//
+// Last Modified By : Administrator
+// Last Modified On : 2020-03-08
+// ***********************************************************************
+// <copyright file="RabbitMqMessageFactory.cs" company="NoobCore.com">
+//     Copyright ©  2020
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kmmp.Core.Extension;
+/// <summary>
+/// The RabbitMQ namespace.
+/// </summary>
+namespace Kmmp.Core.MqFramework.RabbitMQ
+{
+    /// <summary>
+    /// Class RabbitMqMessageFactory.
+    /// </summary>
+    public class RabbitMqMessageFactory
+    {
+        /// <summary>
+        /// Gets the connection factory.
+        /// </summary>
+        /// <value>The connection factory.</value>
+        public ConnectionFactory ConnectionFactory { get; private set; }
+        /// <summary>
+        /// The retry count
+        /// </summary>
+        private int retryCount;
+        /// <summary>
+        /// Gets or sets the retry count.
+        /// </summary>
+        /// <value>The retry count.</value>
+        /// <exception cref="ArgumentOutOfRangeException">RetryCount - Rabbit MQ RetryCount must be 0-1</exception>
+        public int RetryCount
+        {
+            get => retryCount;
+            set
+            {
+                if (value < 0 || value > 1)
+                    throw new ArgumentOutOfRangeException(nameof(RetryCount),
+                        "Rabbit MQ RetryCount must be 0-1");
+
+                retryCount = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether [use polling].
+        /// </summary>
+        /// <value><c>true</c> if [use polling]; otherwise, <c>false</c>.</value>
+        public bool UsePolling { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RabbitMqMessageFactory"/> class.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        /// <param name="username">The username.</param>
+        /// <param name="password">The password.</param>
+        /// <exception cref="ArgumentNullException">connectionString</exception>
+        public RabbitMqMessageFactory(string connectionString = "localhost",
+            string username = null, string password = null)
+        {
+            if (connectionString == null)
+                throw new ArgumentNullException(nameof(connectionString));
+
+            ConnectionFactory = new ConnectionFactory
+            {
+                RequestedHeartbeat = 10,
+            };
+
+            if (username != null)
+                ConnectionFactory.UserName = username;
+            if (password != null)
+                ConnectionFactory.Password = password;
+
+            if (connectionString.StartsWith("amqp://") || connectionString.StartsWith("amqps://"))
+            {
+                ConnectionFactory.Uri = new Uri(connectionString);
+            }
+            else
+            {
+                var parts = connectionString.SplitOnFirst(':');
+                var hostName = parts[0];
+                ConnectionFactory.HostName = hostName;
+
+                if (parts.Length > 1)
+                {
+                    ConnectionFactory.Port = parts[1].ToInt();
+                }
+            }
+        }
+    }
+}
